ceeb67937dd5bc8c9bb7c67b232db4894be0d7c8
unknown <bal23@CS-348-22-WIN.argo.uwf.edu>
2017-01-31 20:00:34 -0600

    Adding link generation based on links from Yang.

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 var express = require('express');
-
+var request = require('request');
 var app = express();
 
 // set up handlebars view engine
@@ -7,6 +7,7 @@ var handlebars = require('express-handlebars')
     .create({
         defaultLayout: 'main'
     });
+
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 
@@ -14,16 +15,51 @@ app.set('port', process.env.PORT || 3000);
 
 app.use(express.static(__dirname + '/public'));
 
-
 app.get('/', function(req, res) {
+    var name1 = getUsername();
+    var name2 = getUsername();
+
+    var link1 = createLink(name1);
+    var link2 = createLink(name2)
+
     res.render('home', {
-        links: ['link1', 'link2']
+        links: [link1, link2]
     });
 });
 
-app.get('/admin', function(req, res) {
-    res.redirect('/404');
-});
+function createLink(username) {
+    var uri = "https://secure.runescape.com/m=account-creation/g=oldscape/create_account?trialactive=true&displayname=" + username + "&email1=" + username + "@aug.dog&age=19&password1=augdog1&password2=augdog1";
+    return uri;
+}
+
+function getUsername() {
+    // Set the headers
+    var headers = {
+        'User-Agent': 'Mars',
+        'Content-Type': 'application/form-data'
+    }
+
+    // Configure the request
+    var options = {
+        url: 'http://aug.dog/yang',
+        method: 'POST',
+        headers: headers
+    }
+
+    // Start the request
+    request(options, function(error, response, body) {
+        if (!error && response.statusCode == 200) {
+            // Print out the response body
+            console.log("Logging inside the request " + JSON.parse(body).name);
+            var parsedBody = JSON.parse(body);
+            var name = parsedBody.name;
+            console.log(name);
+            return name;
+        } else {
+            console.log(error);
+        }
+    })
+}
 
 // 404 catch-all handler (middleware)
 app.use(function(req, res, next) {
